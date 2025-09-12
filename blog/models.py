ac4e2eab9d7370b7d050d369fe01afa8419d7bf1
PeterCones <olly.john@live.co.uk>
2025-09-12 07:44:58 +0000

    Add comments feature to post detail view and template

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_title")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_author")
     body = models.TextField()
     approved = models.BooleanField(default=False)
